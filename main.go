f17942ce7043c2f3c245411697d492fa43b37ced
Bnag <uladzislau.samuseu@gmail.com>
2023-05-23 22:57:36 +0300

    custom routing with url path and not found page

--- main.go
+++ main.go
@@ -13,12 +13,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func contactFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:uladzislau.samuseu@gmail.com\">uladzislau.samuseu@gmail.com</a>.")
+}
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactFunc(w, r)
+	default:
+		http.NotFound(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactFunc)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
