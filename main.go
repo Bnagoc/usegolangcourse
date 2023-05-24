36dafae3b87e4f9978308e05ff14b239161c0492
Bnag <uladzislau.samuseu@gmail.com>
2023-05-24 19:14:10 +0300

    creating additional page(exercise)

--- main.go
+++ main.go
@@ -15,6 +15,17 @@ func contactFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:uladzislau.samuseu@gmail.com\">uladzislau.samuseu@gmail.com</a>.")
 }
 
+func faqFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li><b>Is there a free version?</b> Yes! we offer a free trial for 30 days on any paid plans.</li>
+		<li><b>What are your support hours?</b> We have support staff answering emails 24/7, though responce times may be a bit slower on weekends.</li>
+		<li><b>How do I contact support?</b> Email us - <a href="mailto:uladzislau.samuseu@gmail.com">uladzislau.samuseu@gmail.com</a>.</li>
+	</ul>
+	`)
+}
+
 type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +34,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactFunc(w, r)
+	case "/faq":
+		faqFunc(w, r)
 	default:
 		http.NotFound(w, r)
 	}
