17294fffc685aeeae669f06c3694ed2d68bd1d83
Bnag <uladzislau.samuseu@gmail.com>
2023-06-04 17:09:15 +0300

    creating a Signup Page

--- main.go
+++ main.go
@@ -25,6 +25,10 @@ func main() {
 		views.Must(views.ParseFS(templates.FS,
 			"faq.gohtml", "tailwind.gohtml"))))
 
+	r.Get("/signup", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS,
+			"signup.gohtml", "tailwind.gohtml"))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
