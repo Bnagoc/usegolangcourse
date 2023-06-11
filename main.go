179c22bd164acfddc5edc7686df9651a44eff976
Bnag <uladzislau.samuseu@gmail.com>
2023-06-11 22:23:50 +0300

    created a Signin Page

--- main.go
+++ main.go
@@ -42,7 +42,12 @@ func main() {
 
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS,
 		"signup.gohtml", "tailwind.gohtml"))
+
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS,
+		"signin.gohtml", "tailwind.gohtml"))
+
 	r.Get("/signup", usersC.New)
+	r.Get("/signin", usersC.SignIn)
 	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
